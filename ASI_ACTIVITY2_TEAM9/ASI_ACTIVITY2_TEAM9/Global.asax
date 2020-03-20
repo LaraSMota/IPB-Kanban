5888d53b3d304ac6945850231c3ae0b54408f79d
LucasMaltauro <maltaurolucas@gmail.com>
2020-03-20 20:03:04 +0000

    importacao inicial

---
+++ ASI_ACTIVITY2_TEAM9/ASI_ACTIVITY2_TEAM9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASI_ACTIVITY2_TEAM9.MvcApplication" Language="C#" %>
---
+++ ASI_ACTIVITY2_TEAM9/ASI_ACTIVITY2_TEAM9/Models/Board.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASI_ACTIVITY2_TEAM9.Models
+{
+    public class Board
+    {
+    }
+}
