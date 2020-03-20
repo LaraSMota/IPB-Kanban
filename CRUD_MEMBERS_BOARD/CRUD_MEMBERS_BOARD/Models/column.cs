8c8d58b0ecc75d73a64547264c3792528771b1f0
Aline <alineslopes.4@gmail.com>
2020-03-20 22:03:23 +0000

    column

---
+++ CRUD_MEMBERS_BOARD/CRUD_MEMBERS_BOARD/Models/column.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MEMBERS_BOARD.Models
+{
+    public class column
+    {
+        [Key]
+        public int id_column { get; set; }
+        public string Title { get; set; }
+        public int BoardId { get; set; }
+    }
+}
--- CRUD_MEMBERS_BOARD/CRUD_MEMBERS_BOARD/Models/context.cs
+++ CRUD_MEMBERS_BOARD/CRUD_MEMBERS_BOARD/Models/context.cs
@@ -9,5 +9,6 @@ namespace CRUD_MEMBERS_BOARD.Models
     public class context : DbContext
     {
         public DbSet<member> Members { get; set; }
+        public DbSet<column> Columns { get; set; }
     }
 }
