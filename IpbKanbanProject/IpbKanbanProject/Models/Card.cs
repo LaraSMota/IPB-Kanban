469119fca84623f8dac87a81c78394567043c5cd
LaraSMota <lara.s.mota@gmail.com>
2020-03-23 22:23:08 -0300

    Another solution for the crud problem

---
+++ IpbKanbanProject/IpbKanbanProject/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpbKanbanProject.Models
+{
+    public class Card
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Comments { get; set; }
+        public string Attachments { get; set; }
+        public string Labels { get; set; }
+        public DateTime DueDate { get; set; }
+        public int ImportantState { get; set; }
+        public string Checklist { get; set; }
+    }
+}
---
+++ IpbKanbanProject/IpbKanbanProject/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IpbKanbanProject.Models
+{
+
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
