2b8d9366da3ee3f35483416a84a12b4d8672404c
LucasMaltauro <maltaurolucas@gmail.com>
2020-05-14 10:44:29 +0100

    update 05_14

---
+++ Back-End/Back-End/Model/Collumn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Back_End.Model
+{
+    public partial class Collumn
+    {
+        public Collumn()
+        {
+            Card = new HashSet<Card>();
+        }
+
+        public int CollumnId { get; set; }
+        public string Title { get; set; }
+        public int BoardId { get; set; }
+
+        public virtual Board Board { get; set; }
+        public virtual ICollection<Card> Card { get; set; }
+    }
+}
---
+++ Back-End/Back-End/Model/UsersCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Back_End.Model
+{
+    public partial class UsersCard
+    {
+        public int CardId { get; set; }
+        public int UsersId { get; set; }
+
+        public virtual Card Card { get; set; }
+        public virtual Users Users { get; set; }
+    }
+}
